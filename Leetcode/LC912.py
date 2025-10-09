2a10382aabe3fb4081c83ce462ce603e3731b1fb
Kumar Mohit <kumarmohitsspn969@gmail.com>
2025-10-10 01:08:12 +0530

    Leetcode Problem 912

--- Leetcode/LC912.py
+++ Leetcode/LC912.py
@@ -1,41 +1,37 @@
-# 912. Sort an Array
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
 
-        def merge(nums,low,mid,high):
-            temp=[]
-            while(left<=mid and right<=high):
-                if nums[left]<=nums[right]:
-                    temp.append(nums[left])
-                    left+=1
-                else:
-                    temp.append(nums[right])
-                    right+=1
-            while(left<len(nums)):
-                if nums[left]<=nums[right]:
-                    temp.append(nums[left])
-                    left+=1
-            while(right<len(nums)):
-                if nums[left]<=nums[right]:
-                    temp.append(nums[right])
-                    right+=1
-            for i in range(low,high):
-                nums[i]= temp[i - low] 
-
-            return nums      
-        
-        
-    low = 0
-    high = len(nums)-1    
-        
-    def mergeS(nums,low,high):
-        if low==high:
+    def merge(self,nums,low,mid,high):
+        temp=[]
+        left = low
+        right=mid+1
+        while(left<=mid and right<=high):
+            if nums[left]<=nums[right]:
+                temp.append(nums[left])
+                left+=1
+            else:
+                temp.append(nums[right])
+                right+=1
+        while(left<=mid):
+            # if nums[left]<=nums[right]:
+            temp.append(nums[left])
+            left+=1
+        while(right<=high):
+            # if nums[left]<=nums[right]:
+            temp.append(nums[right])
+            right+=1
+        for i in range(low,high+1):
+            nums[i]= temp[i - low] 
+            
+    def mergeS(self,nums,low,high):
+        if low>=high:
             return
-
-        mid = low+high//2
-        mergeS(nums,low,mid)
-        mergeS(nums,mid+1,high)
-        merge(nums,low,mid,high)
-
-
-        
+        mid = (low+high)//2
+        self.mergeS(nums,low,mid)
+        self.mergeS(nums,mid+1,high)
+        self.merge(nums,low,mid,high)  
+        # return nums
+    def sortArray(self, nums: List[int]) -> List[int]:
+        low = 0
+        high = len(nums)-1
+        self.mergeS(nums,low,high)
+        return nums         
