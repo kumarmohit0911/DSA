82a182679296856177b41a10dd0c8d8b39e6f713
Kumar Mohit <kumarmohitsspn969@gmail.com>
2025-10-12 21:18:35 +0530

    Problem 704 Leetcode

---
+++ Leetcode/LC704.py
@@ -0,0 +1,8 @@
+# Binary Search
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
