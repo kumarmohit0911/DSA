025f0becbca74d5e38bf0a6e62f60f16473a2a4a
Kumar Mohit <kumarmohitsspn969@gmail.com>
2025-10-13 23:51:25 +0530

    Problen 193  of LC

---
+++ Leetcode/LC193.py
@@ -0,0 +1,9 @@
+# Minimum of a rotated array
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        minm = nums[0]
+        for i in nums:
+            if i <=minm:
+                minm=i
+        return minm
