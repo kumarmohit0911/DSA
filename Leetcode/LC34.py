f1e5e509d7ab37cd77b3160b5b53f6445143a4b4
Kumar Mohit <kumarmohitsspn969@gmail.com>
2025-10-13 23:53:12 +0530

    Problen 33  of LC

---
+++ Leetcode/LC34.py
@@ -0,0 +1,9 @@
+#  Search in rotated array
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else :
+            return -1
+        
